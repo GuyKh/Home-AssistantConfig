cc38e15fc03e0e60e67ae46023cfe2c16da694c0
Guy Khmelnitsky <guykhmel@gmail.com>
2019-06-06 21:56:53 +0300

    Extract Cameras Package and HACS package updates (#26)
    * Add Maria DB Size Graph
    
    * Current Changes
    
    * Current Changes
    
    * Undo redundant Changes
    
    * HACS package updates
    
    * Delete __init__.cpython-37.pyc
    
    * HA 0.94 and Watson TTS

--- custom_components/hacs/frontend/views/store.py
+++ custom_components/hacs/frontend/views/store.py
@@ -1,64 +1,103 @@
-"""CommunityAPI View for HACS."""
+"""Serve HacsStoreView."""
+# pylint: disable=broad-except
 import logging
 from aiohttp import web
-from homeassistant.components.http import HomeAssistantView
+from ...blueprints import HacsViewBase
 
-from custom_components.hacs.frontend.views import error_view
-from custom_components.hacs.frontend.views.overview import overview
-from custom_components.hacs.frontend.elements import style, header
+_LOGGER = logging.getLogger('custom_components.hacs.frontend')
 
 
-_LOGGER = logging.getLogger(__name__)
+class HacsStoreView(HacsViewBase):
+    """Serve HacsOverviewView."""
 
-
-class CommunityStore(HomeAssistantView):
-    """View to serve the overview."""
-
-    requires_auth = False
-
-    url = r"/community_store"
     name = "community_store"
 
-    def __init__(self, hass):
-        """Initialize overview."""
-        self.hass = hass
+    def __init__(self):
+        """Initilize."""
+        self.url = self.url_path["store"]
 
     async def get(self, request):  # pylint: disable=unused-argument
-        """View to serve the overview."""
+        """Serve HacsStoreView."""
         try:
-            html = await self.store_view()
-        except Exception as error:  # pylint: disable=broad-except
-            _LOGGER.error(error)
-            html = await error_view()
-        return web.Response(body=html, content_type="text/html", charset="utf-8")
+            content = self.base_content
 
-    async def store_view(self):
-        """element_view."""
-        content = ""
-        content += await style()
-        content += await header()
-        content += """
-        <script>
-            function Search() {
-            var input = document.getElementById("Search");
-            var filter = input.value.toLowerCase();
-            var nodes = document.getElementsByClassName('row');
+            integrations = []
+            plugins = []
 
-            for (i = 0; i < nodes.length; i++) {
-                if (nodes[i].innerText.toLowerCase().includes(filter)) {
-                nodes[i].style.display = "block";
-                } else {
-                nodes[i].style.display = "none";
-                }
-            }
-            }
-        </script>
-        """
-        content += "<div class='container''>"
-        content += '<input type="text" id="Search" onkeyup="Search()" placeholder="Please enter a search term.." title="Type in a name">'
-        content += "<h5>CUSTOM INTEGRATIONS</h5>"
-        content += await overview(self.hass, "integration")
-        content += "<h5>CUSTOM PLUGINS (LOVELACE)</h5>"
-        content += await overview(self.hass, "plugin")
-        content += "</div>"
-        return content
+            if not self.repositories:
+                content += "Loading store items, check back later."
+
+            else:
+
+                content += """
+                    <div class='hacs-overview-container'>
+                        <input type="text" id="Search" onkeyup="Search()" placeholder="Please enter a search term.." title="Type in a name" autofocus style="color: var(--primary-text-color)">
+                    </div>
+                """
+
+                for repository in self.repositories_list_name:
+
+                    if not repository.track or repository.hide:
+                        continue
+
+                    if repository.pending_restart:
+                        card_icon = "<i class='fas fa-cube card-status pending-restart'></i>"
+
+                    elif repository.pending_update:
+                        card_icon = "<i class='fas fa-cube card-status pending-update'></i>"
+
+                    elif repository.installed:
+                        card_icon = "<i class='fas fa-cube card-status installed'></i>"
+
+                    else:
+                        card_icon = "<i class='fas fa-cube card-status default'></i>"
+
+                    card = """
+                    <a href="{}/{}" class="hacs-card"">
+                        <div class="hacs-card overview">
+                            <meta topics="{}">
+                            <meta repository_authors="{}">
+                            <span class="hacs-card-title">{} {}</span>
+                            <span class="hacs-card-content">
+                                <p>{}</p>
+                            </span>
+                        </div>
+                    </a>
+                    """.format(self.url_path["repository"], repository.repository_id, repository.topics, repository.authors, card_icon, repository.name, repository.description)
+
+                    if repository.repository_type == "integration":
+                        integrations.append(card)
+
+                    elif repository.repository_type == "plugin":
+                        plugins.append(card)
+
+                    else:
+                        continue
+
+                if integrations:
+                    content += "<div class='hacs-overview-container'>"
+                    content += "<h5>CUSTOM INTEGRATIONS</h5>"
+                    content += "<div class='hacs-card-container'>"
+                    for card in integrations:
+                        content += card
+                    content += "</div></div>"
+
+                if plugins:
+                    content += "<div class='hacs-overview-container'>"
+                    content += "<h5>CUSTOM PLUGINS (LOVELACE)</h5>"
+                    content += "<div class='hacs-card-container'>"
+                    for card in plugins:
+                        content += card
+                    content += "</div></div>"
+
+                if not plugins and not integrations:
+                    content = self.base_content
+                    content += "Loading store items, check back later."
+
+                content += self.footer
+
+        except SystemError as exception:
+            _LOGGER.error(exception)
+            raise web.HTTPFound(self.url_path["error"])
+
+        return web.Response(body=content, content_type="text/html", charset="utf-8")
